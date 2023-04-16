6b789bcec190f48d572cb5099b6193cb90dba848
David Park <dp247@rocketmail.com>
2023-04-16 23:35:24 +0100

    Add XMLTV generation

--- main.py
+++ main.py
@@ -2,31 +2,85 @@ from lxml import etree
 from datetime import datetime, timedelta, time, timezone
 import json
 import requests
+import pytz
 
 def get_days() -> list[int]:
+    """
+Generate epoch times for now, midnight tomorrow, and midnight the next day
+    :return: List of epoch times
+    """
     now = int(datetime.timestamp(datetime.now()))
     day_1 = int(datetime.timestamp(datetime.combine(datetime.now(), time(0, 0)) + timedelta(1)))
     day_2 = int(datetime.timestamp(datetime.combine(datetime.now(), time(0, 0)) + timedelta(2)))
     return [now, day_1, day_2]
 
 def get_channels_data() -> list:
+    """
+Load XML file of channel information
+    :return: XML elements as a set, then all sets as a list
+    """
     data_list = []
-    # Load in channels xml file
     x = etree.parse('sky.com.channels.xml')
     data = x.find('channels').getchildren()
 
-    # channels_data.append([c.attrib for c in data])
-    for channel in data:
-        if channel.items() is not None:
-            items = channel.items()
-            items.append(('name', channel.text))
+    for element in data:
+        if element.items() is not None:
+            items = element.items()
+            items.append(('name', element.text))
             data_list.append(items)
 
     return data_list
 
+def build_xmltv(channels: list, programmes: list) -> bytes:
+    """
+Make the channels and programmes into something readable by XMLTV
+    :param channels: The list of channels to be generated
+    :param programmes: The list of programmes to be generated
+    :return: A sequence of bytes for XML
+    """
+    # Timezones since UK has daylight savings
+    dt_format = '%Y%m%d%H%M%S %z'
+    tz = pytz.timezone('Europe/London')
+
+    data = etree.Element("tv")
+    data.set("generator-info-name", "sky-epg-parser")
+    for ch in channels:
+        channel = etree.SubElement(data, "channel")
+        channel.set("id", ch[1][1])
+        name = etree.SubElement(channel, "display-name")
+        name.set("lang", "en")
+        name.text = ch[3][1]
+
+    for pr in programmes:
+        programme = etree.SubElement(data, 'programme')
+        start_time = tz.localize(datetime.fromtimestamp(pr.get('start'))).strftime(dt_format)
+        end_time = tz.localize(datetime.fromtimestamp(pr.get('stop'))).strftime(dt_format)
+
+        programme.set("channel", pr.get('channel'))
+        programme.set("start", start_time)
+        programme.set("stop", end_time)
+
+        title = etree.SubElement(programme, "title")
+        title.set('lang', 'en')
+        title.text = pr.get("title")
+
+        if pr.get('description') is not None:
+            description = etree.SubElement(programme, "desc")
+            description.set('lang', 'en')
+            description.text = pr.get("description")
+
+        if pr.get('icon') is not None:
+            icon = etree.SubElement(programme, "icon")
+            icon.set('src', pr.get("icon"))
+
+    return etree.tostring(data, pretty_print=True, encoding='utf-8')
+
+# Load the channels data
 channels_data = get_channels_data()
-programs = []
+
+programme_data = []
 for channel in channels_data:
+    print(channel[1][1])
     epoch_times = get_days()
     for epoch in epoch_times:
         url = f"https://epgservices.sky.com/5.2.2/api/2.0/channel/json/{channel[2][1]}/{epoch}/86400/4"
@@ -37,13 +91,13 @@ for channel in channels_data:
         epg_data = result['listings'][f'{channel[2][1]}']
         for item in epg_data:
             title = item['t']
-            desc = item['d'] if hasattr(item, 'd') else None
+            desc = item['d'] if 'd' in item else None
             start = int(item['s'])
             end = int(item['s']) + int(item['m'][1])
-            icon = f"http://epgstatic.sky.com/epgdata/1.0/paimage/46/1/{item['image']}" if hasattr(item, 'image') else None
+            icon = f"http://epgstatic.sky.com/epgdata/1.0/paimage/46/1/{item['image']}" if 'image' in item else None
             ch_name = channel[1][1]
 
-            programs.append({
+            programme_data.append({
                 "title": title,
                 "description": desc,
                 "start": start,
@@ -52,7 +106,9 @@ for channel in channels_data:
                 "channel": ch_name
             })
 
+channel_xml = build_xmltv(channels_data, programme_data)
 
+with open('test.xml', 'wb') as f:
+    f.write(channel_xml)
+    f.close()
 
-
-print('')
