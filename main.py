34158aab01dcf315605c1ba3a0a771bbe1277f4b
David Park <dp247@rocketmail.com>
2023-06-26 22:31:09 +0100

    Remove traces of BS4

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta, time, timezone
 import json
 import requests
 import pytz
-from bs4 import BeautifulSoup
 
 bt_dt_format = '%Y-%m-%dT%H:%M:%SZ'
 tz = pytz.timezone('Europe/London')
@@ -158,86 +157,6 @@ for channel in channels_data:
                     "icon": icon,
                     "channel": ch_name
                 })
-
-    # If EPG is a BBC radio station
-    if channel[0][1] == "bbc_radio":
-        url_list = [f'https://www.bbc.co.uk/sounds/schedules/{channel[3][1]}/{d.date()}' for d in get_days("bbc_radio")]
-        for url in url_list:
-            current_date = datetime.strptime(url.split('/')[-1], "%Y-%m-%d").date()
-            response = requests.get(url)
-            if response.status_code != 200:
-                continue
-            soup = BeautifulSoup(response.content, features="html.parser")
-            soup.prettify()
-
-            # Get the schedule sections (early, morning, afternoon, evening, late)
-            sections = soup.find_all("section", {"class": "sc-c-schedule-segment"})
-            for s_idx, section in enumerate(sections):
-                # Find each programme item as a block on the schedule
-                programme_items = section.find_all("div", {
-                    "class": "sc-c-schedule-item gs-u-display-block gs-u-mb gs-u-mb+@m gs-u-pv+"})
-                print(f"Total items in section {s_idx}: {len(programme_items)}")
-                for p_idx, item in enumerate(programme_items):
-                    # Get the air time from the block. We only want shows from 00:00:00 to 23:59:59 for each page,
-                    # so get rid of shows that start on the day before or day after
-                    air_time = datetime.strptime(
-                        item.find("p", {"class": "sc-c-schedule-item__on-air-time gel-great-primer"}).text,
-                        "%H:%M").time()
-                    if section.attrs.get("aria-labelledby") == "early":
-                        if air_time >= time(7, 0, 0):
-                            continue
-                    if section.attrs.get("aria-labelledby") == "late":
-                        if air_time > time(0, 30, 0):
-                            continue
-
-                    # Dive further into each item to extract info correctly
-                    programme_thumbnail = item.find("img")['src']
-                    info = item.find("div", {"class": "gs-u-display-flex sc-u-flex-column"}).contents[0]
-                    programme_name = info.contents[0].text
-                    programme_desc = info.contents[2].text
-                    programme_start = datetime.combine(current_date, air_time)
-                    if section.attrs.get("aria-labelledby") == "late":
-                        if programme_start.time() > time(0, 30, 0):
-                            continue
-                    ch_name = channel[2][1]
-                    q = len(programme_items)
-                    # If we're still within the same section (as in, we haven't run out of programmes in the list), get
-                    # the next programme's start time from the next item in the list
-                    try:
-                        if p_idx <= len(programme_items):
-                            next_idx = p_idx + 1
-                            next_start = datetime.strptime(programme_items[next_idx].find("p", {
-                                "class": "sc-c-schedule-item__on-air-time gel-great-primer"}).text, "%H:%M").time()
-                    except IndexError as ex:
-                        # However, if we have run out of programmes, but not run out of sections, then find the next
-                        # start time from first programme block in the next section
-                        if s_idx + 1 < len(sections):
-                            next_idx = s_idx + 1
-                            next_start = datetime.strptime(sections[next_idx].find("div", {
-                                "class": "sc-c-schedule-item gs-u-display-block gs-u-mb gs-u-mb+@m gs-u-pv+"}).find("p", {
-                                "class": "sc-c-schedule-item__on-air-time gel-great-primer"}).text, "%H:%M").time()
-
-                    print(f"\nName: {programme_name}, Loc: S{s_idx}P{p_idx}")
-                    if s_idx == 3 and p_idx == len(programme_items) - 1:
-                        programme_stop = datetime.combine(programme_start.date() + timedelta(days=1), next_start)
-                    elif s_idx == 4:
-                        if len(programme_items) == 1 or p_idx == len(programme_items) - 1:
-                            continue
-                        else:
-                            programme_start = datetime.combine(programme_start.date() + timedelta(days=1), air_time)
-                            programme_stop = datetime.combine(programme_start.date(), next_start)
-                    else:
-                        programme_stop = datetime.combine(programme_start, next_start)
-                    print(f"On: {programme_start} - {programme_stop}\n")
-
-                    programme_data.append({
-                        "title": programme_name,
-                        "description": programme_desc,
-                        "start": programme_start.timestamp(),
-                        "stop": programme_stop.timestamp(),
-                        "icon": programme_thumbnail,
-                        "channel": ch_name
-                    })
 
 channel_xml = build_xmltv(channels_data, programme_data)
 
